e9f287b8365589840417cc72854230ad1d816c82
Ángel Ortega <codenode.ortega@gmail.com>
2024-03-07 14:39:46 -0600

    Add environment variables to GitHub Actions workflow and update code to use them

--- exampleForth/HelloWorld/Program.cs
+++ exampleForth/HelloWorld/Program.cs
@@ -4,9 +4,10 @@ using System;
 class Program {
     static void Main() {
         string? myVariable = Environment.GetEnvironmentVariable("NAME");
+        string? myDay = Environment.GetEnvironmentVariable("DAY");
 
-        if (myVariable != null) {
-            Console.WriteLine($"El valor de USERNAME es: {myVariable}");
+        if (myVariable != null && myDay != null) {
+            Console.WriteLine($"El valor de USERNAME es: {myVariable}, y el valor de DAY es: {myDay}");
         } else {
             Console.WriteLine("MY_VARIABLE no está definida.");
         }
