30e326144b695697f059e2b91a377e6c122b61a9
Ángel Ortega <codenode.ortega@gmail.com>
2024-03-07 14:47:08 -0600

    Add SECRETS environment variable to csharp.yml and update Program.cs to include printing the value of SECRETS

--- exampleForth/HelloWorld/Program.cs
+++ exampleForth/HelloWorld/Program.cs
@@ -5,9 +5,11 @@ class Program {
     static void Main() {
         string? myVariable = Environment.GetEnvironmentVariable("NAME");
         string? myDay = Environment.GetEnvironmentVariable("DAY");
+        string? mySecrets = Environment.GetEnvironmentVariable("SECRETS");
+
 
         if (myVariable != null && myDay != null) {
-            Console.WriteLine($"El valor de USERNAME es: {myVariable}, y el valor de DAY es: {myDay}");
+            Console.WriteLine($"El valor de USERNAME es: {myVariable}, y el valor de DAY es: {myDay} y el secreto es: { mySecrets}");
         } else {
             Console.WriteLine("MY_VARIABLE no está definida.");
         }
